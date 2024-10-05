9a6b5c82037d256427618c6d1bb2d44d1e6bbbd1
Desonect <gabriel.jaen@a.fecaf.com.br>
2024-10-05 19:34:18 -0300

    alteração 1

--- update_dependencies.py
+++ update_dependencies.py
@@ -1,6 +1,7 @@
 import os
 def update_dependencies():
     os.system("pip install --upgrade -r requirements.txt")
-print("Dependências atualizadas com sucesso!")
+    print("Dependências atualizadas com sucesso!")
+
 if __name__ == "__main__":
     update_dependencies()
